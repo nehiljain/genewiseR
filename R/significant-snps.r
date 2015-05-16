81dee8a11c6b2b352b802836a55aad1df85c10fe
Nehl Jain <jain.nehil@gmail.com>
2015-05-16 14:22:36 -0300

    mouse pipeline rerun and all functions added, error handling left

--- R/significant-snps.r
+++ R/significant-snps.r
@@ -1,4 +1,4 @@
-
+library(Hmisc)
 
 #' if the out file path is not given then it returns the datatable else writes a tsv on the new path
 get_significant_snps <- function(df, threshold, column_name, out_file_path = NULL) {
@@ -16,7 +16,7 @@ get_significant_snps <- function(df, threshold, column_name, out_file_path = NUL
 #' if the out file path is not given then it returns the datatable else writes a tsv on the new path
 get_nlp <- function(df, column_name, out_file_path = NULL) {
   nlp_column <- paste0(column_name, ".nlp")
-  df[, (nlp_column) := -1 * log(get(column_name))]
+  df[, (nlp_column) := -1 * log10(get(column_name))]
   if (is.null(out_file_path)) {
     return(df)
   } else {
@@ -30,13 +30,61 @@ get_nlp <- function(df, column_name, out_file_path = NULL) {
 get_max_and_mean <- function(df, column_name, out_file_path = NULL) { 
   max_column <- paste0( "chr_max_",column_name)
   mean_column <- paste0( "chr_mean_",column_name)
-  new_column_names <- c(,max_column, mean_column)
-  df [, (max_column) := max(get(column_name)), by="ensembl_gene_id"]
-  df [, (mean_column) := mean(get(column_name)),  by="ensembl_gene_id"]
-  df [, snp_count := .N,  by="ensembl_gene_id"]
+  
+  df [, (max_column) := max(get(column_name)), by="ensemble_gene_id"]
+  df [, (mean_column) := mean(get(column_name)),  by="ensemble_gene_id"]
+  df [, snp_count := .N,  by="ensemble_gene_id"]
+  #tricky step
+  df <- unique(df[,.(ensemble_gene_id, maxT = get(max_column), meanT = get(mean_column), snp_count)])
+  
   if (is.null(out_file_path)) {
     return(df)
   } else {
     write.table(x = df, file=out_file_path, quote = F, sep = "\t", row.names = F)
+  }
+}
+
+
+
+
+get_quartile <- function(df, column_name) {
+  df <- as.data.frame(df)
+  if (dim(df)[1] == 1) {
+    
+    df$quartile <- df[,column_name]
+    
+  } else{
+#     cat("Before Quartile", str(df))
+    df$quartile <- with(df, cut(get(column_name),
+                                breaks=unique(quantile(get(column_name), probs=seq(0,1, by=0.25))), 
+                                include.lowest=TRUE))
+    df$quartile <- as.factor(df$quartile)
+    last_quartile <- tail(levels(df$quartile), n=1)
+#     cat("\n\nlast quartile\n\n",last_quartile)
+#     cat("\n\nlevels of quartiles\n\n",levels(df$quartile))
+    df$quartile_ch <- as.character(df$quartile)
+    df <- filter(df, as.character(quartile_ch) == as.character(last_quartile))
+#     cat("\n\ndf after fildering\n\n",str(df))
+  }
+  df <- as.data.table(df)
+  cat("\n\nbefore mean of all\n\n",str(df))
+  df[, topQ_nlp := mean(get(column_name), na.rm = TRUE)]
+  df <- df[, .(ensemble_gene_id, topQ_nlp)]
+  return(df)
+}
+
+
+get_topQ <- function(df, column_name, out_file_path = NULL) {
+  result_sign_snp_topq_df <- ddply(df, "ensemble_gene_id", function(df) {
+    #   print(str(df))
+    return(get_quartile(df, column_name))
+  })
+  
+  result_sign_snp_topq_df <- unique(result_sign_snp_topq_df)
+  str(result_sign_snp_topq_df)
+  if (is.null(out_file_path)) {
+    return(result_sign_snp_topq_df)
+  } else {
+    write.table(x = result_sign_snp_topq_df, file=out_file_path, quote = F, sep = "\t", row.names = F)
   }
 }
--- R/snps-to-genes.r
+++ R/snps-to-genes.r
@@ -3,19 +3,20 @@ library(data.table)
 library(stringr)
 
 #NOTE: NEHIL NEEDS TO CHANGE NORM_VAR_NAMES FUNCTION
+#NOTE: Nehil remove your fake gene start and end columns and cleanup names before writing in all cases. 
 
 
 execute <- function (in_genome_data_file_path, sep1, 
                      in_ref_gene_id_file_path, sep2, 
                      out_snps_in_genes_file_path, sep3, 
                      window_size = 1000) {
-  chr_list <- as.character(c(1:29,"X"))
+  chr_list <- as.character(c(1:29,"X","Y"))
 
   genome_data <- fread(in_genome_data_file_path, sep=sep1, sep2="auto", header=T, na.strings="NA",
                        stringsAsFactors=FALSE, verbose=TRUE)  
 #   setnames(genome_data,names(genome_data),norm_var_names(names(genome_data)))
-  col_names <- c("chr_no","source","feature_type","gene_start_bp","gene_end_bp")
-  genome_data[,chr_no := as.factor(chr_no)]
+
+#   genome_data[,chr_no := as.factor(chr_no)]
   
   ref_gene_id_data <- fread(in_ref_gene_id_file_path, sep=sep2, sep2="auto", na.strings="NA",
                           stringsAsFactors=FALSE, verbose=TRUE)
@@ -24,12 +25,13 @@ execute <- function (in_genome_data_file_path, sep1,
            names(ref_gene_id_data), 
            c("ensemble_gene_id","description", "chromosome_name", "gene_start_(bp)", "gene_end_(bp)", "strand","band"
              ,"associated_gene_name", "associated_gene_source","gene_biotype","source","status","version"))
-#   print(names(ref_gene_id_data))
+  print(names(ref_gene_id_data))
   ref_gene_id_data <- ref_gene_id_data[chromosome_name %in% chr_list] 
-  ref_gene_id_data[,chromosome_name := as.factor(chromosome_name)]
+#   ref_gene_id_data[,chromosome_name := as.factor(chromosome_name)]
 
   setnames(ref_gene_id_data, c("chromosome_name","gene_start_(bp)", "gene_end_(bp)"), c("chr_no","gene_start", "gene_end"))
   result_dt <- map_snps_to_gene(genome_data, ref_gene_id_data, window_size)
+  result_dt[,c("fake_gene_start","fake_gene_end","i.fake_gene_start","i.fake_gene_end"):= NULL]
   write.table(result_dt, out_snps_in_genes_file_path, sep=sep3, row.names=F, quote = F)
 }
 
@@ -43,14 +45,21 @@ map_snps_to_gene <- function(genome_dt, ref_dt, window_size) {
   genome_dt[, fake_gene_start := snp_pos]
   genome_dt[, fake_gene_end := snp_pos]
   ref_dt[, fake_gene_start := (gene_start - window_size)]
-  ref_dt[, fake_gene_end := (gene_start + window_size)]
+  ref_dt[, fake_gene_end := (gene_end + window_size)]
   setkey(ref_dt, chr_no, fake_gene_start, fake_gene_end)
   result_dt <- foverlaps(genome_dt, ref_dt, type="within", nomatch = 0L)
   return(result_dt)
 } 
 
+in_genome_data_file_path = "/home/kasia/coderepo/genewise/p_adjusted_genome.gwas"
+sep1 = ","
+in_ref_gene_id_file_path = "/home/data/tmp/mouse_gene_list-GRCm38.77-mm10.txt"
+sep2 = ","
+out_snps_in_genes_file_path="/home/data/tmp/all_snps_in_genes.tsv"
+sep3="\t"
 
-# 
+
+
 # execute( in_genome_data_file_path = "/home/data/nehil_genome_p_adj_snp_annotated_study.tsv",
 # in_ref_gene_id_file_path = "/home/data/reference/77/cattle_gene_list-UMD3.1.77.txt",
 # out_snps_in_genes_file_path = "/home/data/cattle_gene_level.tsv")
