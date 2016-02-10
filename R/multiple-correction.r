442d513ad0b8779c75a3cd623f674af0e7acdfe6
Nehil <nehiljain@Nehils-MacBook-Pro-2.local>
2016-02-09 20:24:26 -0400

    multilpe correction function combined for genome and chromosome and made more robust

--- R/multiple-correction.r
+++ R/multiple-correction.r
@@ -10,41 +10,60 @@
 #' @param out_file_path Optional parameter. The file path to store the output.
 #' @return DataTable with additional columns for genomewide correction of each column vector
 
-p_adjustment_genomewide <- function (in_un_adj_p_val_snps_data_file_path, col_names , out_file_path=NULL ) {
+p_adjustment <- function (study_df, 
+                                     col_names,
+                                     level = "genome",
+                                     test = "fdr",
+                                     out_file_path=NULL ) {
   
+  if (!(col_names %in% names(study_df))) {
+    stop(paste0("The column 'chr_no' does not exist in the dataframe.
+                Please refer to documentation"))
+  }
+  
+  if(!(level %in% c("genome","chromosome") )) {
+    stop(paste0("The value of level is not correct.
+                Please refer to documentation"))
+  }
+  
+  if(!data.table::is.data.table(study_df)) {
+    study_df <- data.table::data.table(study_df)
+  }
 
-  
-  assert_that(is.readable(in_un_adj_p_val_snps_data_file_path))
-  snp_stats_dt <- fread(in_un_adj_p_val_snps_data_file_path, sep="\t", sep2="auto", header=T, na.strings="NA",
-                          stringsAsFactors = FALSE, verbose =T)
-  
-  if (!is.integer(snp_stats_dt[,chr])) {
-    max_int_chr <- snp_stats_dt[!is.na(as.numeric(chr)), chr]
-    max_int_chr <- max(as.numeric(max_int_chr))
-    snp_stats_dt[tolower(chr) == tolower('X'), chr := (max_int_chr + 1)]
-    snp_stats_dt[tolower(chr) == tolower('Y'), chr := (max_int_chr + 2)]
-    snp_stats_dt[, chr := as.integer(chr)]
-    if (!is.integer(snp_stats_dt[,chr])) {
-      stop(paste0("The columns 'chr' should be integer, change 'X', 'Y' to numbers and remove any other characters in the column values"))
+  if (!is.integer(study_df[,chr_no])) {
+    max_int_chr_no <- study_df[!is.na(as.numeric(chr_no)), chr_no]
+    max_int_chr_no <- max(as.numeric(max_int_chr_no))
+    study_df[tolower(chr_no) == tolower('X'), chr_no := (max_int_chr_no + 1)]
+    study_df[tolower(chr_no) == tolower('Y'), chr_no := (max_int_chr_no + 2)]
+    study_df[, chr_no := as.integer(chr_no)]
+    if (!is.integer(study_df[,chr_no])) {
+      stop(paste0("The columns 'chr_no' should be integer, change 'X', 'Y' to numbers and remove any other characters in the column values"))
     }
   }
   
-  expect_true( col_names %in% names(snp_stats_dt), info = "The column names are not present in the datatable", label = NULL)
-  length_dt <- nrow(snp_stats_dt)
+  expect_true( col_names %in% names(study_df), 
+               info = "The column names are not present in the datatable", 
+               label = NULL)
+  length_dt <- nrow(study_df)
   for (ch in col_names) {
-    adj_name <- paste0(ch,".adj_genome_wide")
+    adj_name <- paste0(ch,".adj_",level,"_wide")
     flog.debug(sprintf("Col being adjusted @ genomewide scale %s and Output Col - %s", ch, adj_name))
-    snp_stats_dt[, (adj_name) := p.adjust(get(ch), "fdr", length_dt)]
+    
+    if (level == "genome") {
+      study_df[, (adj_name) := p.adjust(get(ch), test, length_dt)]  
+    } else if (level == "chromosome") {
+      study_df[, (adj_name) := p.adjust(get(ch), test, length_dt), by = chr_no]
+    }
   }
   
-  setnames(snp_stats_dt, names(snp_stats_dt), norm_var_names(names(snp_stats_dt)))
+  setnames(study_df, names(study_df), norm_var_names(names(study_df)))
 
   if (!is.null(out_file_path)) {
     assert_that(is.writeable(out_file_path))
     flog.debug(sprintf("Output File path %s", out_file_path))
-    write.table(x = snp_stats_dt, file=out_file_path, quote = F, sep = "\t", row.names = F)
+    write.table(x = study_df, file=out_file_path, quote = F, sep = "\t", row.names = F)
   }
-  return(snp_stats_dt)
+  return(study_df)
  
 }
 
@@ -72,37 +91,6 @@ p_adjustment_summary <- function(df, chr_name, snp_id_name, snp_pos_name, p_val_
   dev.copy(pdf,"p-val-ajustment-summary.pdf", width=4, height=4)
   dev.off()
   
-}
-
-
-
-
-
-
-#' This function add a new column to the datatable created by bonferroni correction
-#' the column name is p_adj_genome. COLUMN required cmh_p_val  
-#' @param SNP p value stats data table
-#' @return Datable with additional columns for genomewide correction of each column vector
-
-p_adjustment_chrwide <- function (in_un_adj_p_val_snps_data_file_path, out_file_path=NULL, col_names = c("cmh_p_val"), p_adj_method = "fdr") {
-  assert_that(is.readable(in_un_adj_p_val_snps_data_file_path))
-  assert_that(is.writeable(out_file_path))
-  
-  snp_stats_dt <- fread(in_un_adj_p_val_snps_data_file_path, sep="\t", sep2="auto", header=T, na.strings="NA",
-                        stringsAsFactors = FALSE, verbose =T)
-  
-  expect_true( col_names %in% names(snp_stats_dt), info = "The column names are not present in the datatable", label = NULL)
-  length_dt <- nrow(snp_stats_dt)
-  for (ch in col_names) {
-    adj_name <- paste0(ch,".p_adj_chr_wide")
-    flog.debug(sprintf("Col being adjusted @ chromosomewide scale %s and Output Col - %s", ch, adj_name))
-    snp_stats_dt[, (adj_name) := p.adjust(get(ch), p_adj_method, length_dt), by = chr_no]
-  }
-  if (!is.null(out_file_path)) {
-    flog.debug(sprintf("Output File path %s", out_file_path))
-    write.table(x = snp_stats_dt, file=out_file_path, quote = F, sep = "\t", row.names = F)
-  }
-  return(snp_stats_dt)
 }
 
 
